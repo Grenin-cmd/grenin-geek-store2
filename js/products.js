24224102b9a3c1181551b80ccb0c48285100c336
Grenin <victorblack2005@gmail.com>
2026-07-23 16:34:56 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,6 +1,6 @@
  // Lista de produtos (EXEMPLO — troque nome, categoria, código, preço e descrição)
   const PRODUCTS = [
-    { name: "Box Mega Luar Clefable", category: "pokemon-tcg", price: 125, desc: "Caixa fechada, 8 pacotes." },
+    { name: "Box Mega Luar Clefable", category: "pokemon-tcg", price: 125, desc: "Caixa fechada, 8 pacotes.", image:"assets/clefable-box.jpg" },
     { name: "Coleção Arco-Iris Evolções Prismaticas", category: "pokemon-tcg", price: 210, desc: "Caixa fechada, 10 pacotes" },
     { name: "Blister Triplo Escuridão Absoluta", category: "pokemon-tcg", price: 42.50, desc: "3 pacotes." },
     { name: "Blister Triplo Caos Ascendente", category: "pokemon-tcg", price: 42.50, desc: "3 pacotes" },
